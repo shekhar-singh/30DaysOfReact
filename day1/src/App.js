f72133df098dce8ea4088e8bbbd83d26b3397c98
shekhar-singh <shekh7ar@gmail.com>
2022-09-17 12:02:20 +0530

    display data in a table

---
+++ day1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Movies from "./components/movies";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <main className='container'>
+      <Movies/>
+    </main>
+   
+  );
+}
+
+export default App;
