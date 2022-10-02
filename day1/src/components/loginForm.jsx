ee0b5bd1b70470fff997b1166218ff9eae6b134b
shekhar-singh <shekh7ar@gmail.com>
2022-10-03 01:21:52 +0530

    take input using state

--- day1/src/components/loginForm.jsx
+++ day1/src/components/loginForm.jsx
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
-  password = React.createRef();
+  state = {
+    account: { username: '', password: '' }
+  };
 
-  componentDidMount(){
-    this.username.current.focus();
+
+  // componentDidMount(){
+  //   this.username.current.focus();
+  // };
+
+  handleChange = ({currentTarget: input}) =>{
+    const account = {...this.state.account};
+    account[input.name] = input.value;
+    this.setState({account});
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const username = this.username.current.value;
-    const password = this.password.current.value;
-    console.log('cred: ', username, password);
+    console.log('cred: ');
 
   };
   
   render() {
+    const {account} = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -24,11 +31,23 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input ref={this.username} type="text" id="username" className="form-control" />
+            <input 
+            onChange={this.handleChange}
+            value={account.username}
+            name='username'
+            type="text" 
+            id="username" 
+            className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input ref={this.password} type="password" id="password" className="form-control" />
+            <input 
+            value={account.password}
+            name='password'
+            onChange={this.handleChange}
+            type="password" 
+            id="password" 
+            className="form-control" />
           </div>
 
           <button className="btn btn-primary">Login</button>
