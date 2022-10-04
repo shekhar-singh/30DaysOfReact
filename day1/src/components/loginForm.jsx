6181414fdd26d347610f76545a54ac3ac24e51a4
shekhar-singh <shekh7ar@gmail.com>
2022-10-05 01:03:38 +0530

    added basic form validation using Joi

--- day1/src/components/loginForm.jsx
+++ day1/src/components/loginForm.jsx
@@ -93,7 +93,7 @@ class LoginForm extends Component {
           error={errors.password}
           />
 
-          <button className="btn btn-primary">Login</button>
+          <button  disabled={this.validate()} className="btn btn-primary">Login</button>
         </form>
       </div>
     );
