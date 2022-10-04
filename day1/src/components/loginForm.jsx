8e2fee942bdf6a15c9aff684804b158e7a235c73
shekhar-singh <shekh7ar@gmail.com>
2022-10-05 01:02:06 +0530

    added basic form validation using Joi

--- day1/src/components/loginForm.jsx
+++ day1/src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -7,15 +8,31 @@ class LoginForm extends Component {
     errors: {}
   };
 
-  validate = () => {
-    const errors = {};
+  schema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password')
+  };
 
-    const { account } = this.state;
-    if (account.username.trim() === '') 
-      errors.username = 'Username is required.';
-    if (account.password.trim() === '')
-    errors.password = 'Password is required.';
-    return Object.keys(errors).length === 0 ? null : errors;
+  validate = () => {
+
+    const options = {abortEarly: false};
+    const result = Joi.validate(this.state.account, this.schema, options);
+
+    if (!result.error) return null;
+
+    const errors = {};
+    for (let item of result.error.details)
+      errors[item.path[0]] = item.message;
+    return errors;
+
+    // const errors = {};
+
+    // const { account } = this.state;
+    // if (account.username.trim() === '') 
+    //   errors.username = 'Username is required.';
+    // if (account.password.trim() === '')
+    // errors.password = 'Password is required.';
+    // return Object.keys(errors).length === 0 ? null : errors;
   };
 
 
@@ -24,12 +41,11 @@ class LoginForm extends Component {
   // };
 
   validateProperty = ({name, value}) => {
-    if (name === 'username') {
-      if (value.trim() === '') return 'Username is required'
-    }
-    if (name === 'password') {
-      if (value.trim() === '') return 'Password is required'
-    }
+    const obj = {[name]: value};
+    const schema = {[name]: this.schema[name]};
+    const {error} = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+
   };
 
   handleChange = ({currentTarget: input}) =>{
@@ -45,7 +61,7 @@ class LoginForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const errors = this.validate();
-    console.log('errors',errors); //log error
+
     this.setState({ errors: errors || {} });
     if (errors) return;
     
